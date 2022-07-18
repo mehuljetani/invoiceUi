a47118543d7192edb3ff7504456f8078d2e8db75
mehuljetani <mehuljetani23@gmail.com>
2022-07-18 15:14:31 +0530

    new one ...

--- src/screens/dashboard/invoices/Invoice.js
+++ src/screens/dashboard/invoices/Invoice.js
@@ -13,31 +13,4 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
   },
-  linearGradient: {
-    height: 400,
-    width: 400,
-  },
-  linearGradient2: {
-    height: 20,
-    opacity: 0.6,
-    marginTop: -40,
-  },
-  text: {
-    color: '#fff',
-    fontSize: 40,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  instagramButton: {
-    height: 450,
-  },
-  signUpButton: {
-    margin: 1,
-    width: 200,
-    borderRadius: 10,
-    paddingVertical: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-  },
 });

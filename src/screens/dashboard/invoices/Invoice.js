dc2842b33c821c899a5dec2506573a551e8d4d99
mehuljetani <mehuljetani23@gmail.com>
2022-07-18 15:06:55 +0530

    invoce changed

--- src/screens/dashboard/invoices/Invoice.js
+++ src/screens/dashboard/invoices/Invoice.js
@@ -3,28 +3,7 @@ import React from 'react';
 import LinearGradient from 'react-native-linear-gradient';
 
 const Invoice = () => {
-  return (
-    <View style={styles.container}>
-      <LinearGradient
-        start={{x: 0.85, y: 0}}
-        end={{x: 0.65, y: 1}}
-        colors={[
-          '#F2D642',
-          '#3FC7AD',
-          '#C2E8D9',
-          '#D1ECE1',
-          '#CEEADD',
-          'white',
-        ]}
-        locations={[0.1, 0.39, 0.8, 0.89, 1, 1]}
-        style={{height: 300, width: 400}}></LinearGradient>
-      <LinearGradient
-        style={{height: 90}}
-        start={{x: 1, y: 0}}
-        end={{x: 1, y: 1}}
-        colors={['#CEEADE', '#FFFFFF']}></LinearGradient>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 };
 
 export default Invoice;
